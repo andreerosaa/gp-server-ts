a36a09ebfd98524d366cc32ef11928647cc8eb1a
andreerosaa <andrefsantosrosa@gmail.com>
2024-11-10 12:35:25 +0000

    server start and error routes

---
+++ src/server.ts
@@ -0,0 +1,50 @@
+import http from 'http';
+import express from 'express';
+import './config/loging';
+import { loggingHandler } from './middleware/loggingHandler';
+import { corsHandler } from './middleware/corsHandler';
+import { routeNotFound } from './middleware/routeNotFound';
+import { SERVER, SERVER_HOSTNAME, SERVER_PORT } from './config/config';
+
+export const application = express();
+export let httpServer: ReturnType<typeof http.createServer>;
+
+export const Main = () => {
+	logging.info('----------------------------------------');
+	logging.info('Initializing API');
+	logging.info('----------------------------------------');
+	application.use(express.urlencoded({ extended: true }));
+	application.use(express.json());
+
+	logging.info('----------------------------------------');
+	logging.info('Logging and Configuration');
+	logging.info('----------------------------------------');
+	application.use(loggingHandler);
+	application.use(corsHandler);
+
+	logging.info('----------------------------------------');
+	logging.info('Define Controller Routing');
+	logging.info('----------------------------------------');
+	application.get('/main/healthcheck', (req, res) => {
+		res.status(200).json({ hello: 'world!' });
+	});
+
+	logging.info('----------------------------------------');
+	logging.info('Define Controller Routing');
+	logging.info('----------------------------------------');
+	application.use(routeNotFound);
+
+	logging.info('----------------------------------------');
+	logging.info('Start Server');
+	logging.info('----------------------------------------');
+	httpServer = http.createServer(application);
+	httpServer.listen(SERVER.SERVER_PORT, () => {
+		logging.info('----------------------------------------');
+		logging.info('Server Started: ' + SERVER_HOSTNAME + ':' + SERVER_PORT);
+		logging.info('----------------------------------------');
+	});
+};
+
+export const Shutdown = (callback: any) => httpServer && httpServer.close(callback);
+
+Main();
