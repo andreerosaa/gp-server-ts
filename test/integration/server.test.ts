2f82843722c4416340e24b2a2b8de49457d572ca
andreerosaa <andrefsantosrosa@gmail.com>
2024-11-11 17:25:34 +0000

    jest testing

---
+++ test/integration/server.test.ts
@@ -0,0 +1,20 @@
+import request from 'supertest';
+import { application, Shutdown } from '../../src/server';
+
+describe('Our application', () => {
+	afterAll((done) => {
+		Shutdown(done);
+	});
+
+	it('Starts and has the proper test environment', async () => {
+		expect(process.env.NODE_ENV).toBe('test');
+		expect(application).toBeDefined();
+	}, 10000);
+
+	it('Returns all options allowed to be called by clients (http methods)', async () => {
+		const response = await request(application).options('/');
+
+		expect(response.status).toBe(200);
+		expect(response.headers['access-control-allow-methods']).toBe('PUT, POST, PATCH, DELETE, GET');
+	});
+});
