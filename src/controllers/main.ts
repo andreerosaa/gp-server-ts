5f7838bf97f7e9a6ac07104fc66d7991ae63e367
andreerosaa <andrefsantosrosa@gmail.com>
2024-11-12 10:17:49 +0000

    data validation with JOI

--- src/controllers/main.ts
+++ src/controllers/main.ts
@@ -1,12 +1,25 @@
 import { Request, Response, NextFunction } from 'express';
 import { Controller } from '../decorators/controller';
 import { Route } from '../decorators/route';
+import Joi from 'joi';
+import { Validate } from '../decorators/validate';
+
+const postHealthCheckValidation = Joi.object({
+	name: Joi.string().required(),
+	email: Joi.string().email()
+});
 
 @Controller()
 class MainController {
 	@Route('get', '/healthcheck')
-	@Route('post', '/healthcheck')
 	getHealthCheck(req: Request, res: Response, next: NextFunction) {
+		logging.info('Healthcheck called successfully');
+		return res.status(200).json({ hello: 'world!' });
+	}
+
+	@Route('post', '/healthcheck')
+	@Validate(postHealthCheckValidation)
+	postHealthCheck(req: Request, res: Response, next: NextFunction) {
 		logging.info('Healthcheck called successfully');
 		return res.status(200).json({ hello: 'world!', ...req.body });
 	}
