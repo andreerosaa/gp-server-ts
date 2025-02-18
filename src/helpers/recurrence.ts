1749712260795ab569436f98d25a66a7fcfc353d
andreerosaa <andrefsantosrosa@gmail.com>
2025-02-18 10:41:34 +0000

    computed date reference bug fix

--- src/helpers/recurrence.ts
+++ src/helpers/recurrence.ts
@@ -4,27 +4,27 @@ export const computerDatesByRecurrence = (startDate: Date, recurrence: SessionRe
 	const endDate = new Date(new Date(startDate).getTime() + seriesLengthInDays * 24 * 60 * 60 * 1000);
 
 	let computedDate = new Date(startDate);
-	const computedDates: Date[] = [computedDate];
+	const computedDates: Date[] = [new Date(computedDate)];
 
 	while (computedDate < endDate) {
 		switch (recurrence) {
 			case SessionRecurrenceEnum.DAILY:
 				computedDate = new Date(computedDate.getTime() + 24 * 60 * 60 * 1000);
-				computedDates.push(computedDate);
+				computedDates.push(new Date(computedDate));
 				break;
 			case SessionRecurrenceEnum.WEEKDAYS:
 				computedDate = new Date(computedDate.getTime() + 24 * 60 * 60 * 1000);
 				if (![0, 6].includes(computedDate.getDay())) {
-					computedDates.push(computedDate);
+					computedDates.push(new Date(computedDate));
 				}
 				break;
 			case SessionRecurrenceEnum.WEEKLY:
 				computedDate = new Date(computedDate.getTime() + 7 * 24 * 60 * 60 * 1000);
-				computedDates.push(computedDate);
+				computedDates.push(new Date(computedDate));
 				break;
 			case SessionRecurrenceEnum.MONTHLY:
 				computedDate = new Date(computedDate.setMonth(computedDate.getMonth() + 1));
-				computedDates.push(computedDate);
+				computedDates.push(new Date(computedDate));
 				break;
 		}
 	}
